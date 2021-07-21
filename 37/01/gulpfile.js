23ea539acd6edc08b8c0ecb7cf17933d47fa8a0e
Kainos-SU <kapinos4@gmail.com>
2021-07-22 02:37:21 +0300

    Homework with cards

---
+++ 37/01/gulpfile.js
@@ -0,0 +1,93 @@
+const gulp = require("gulp");
+const rename = require("gulp-rename");
+const sourcemap = require("gulp-sourcemaps");
+const sass = require("gulp-sass")(require("sass"));
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require("browser-sync").create();
+const fs = require("fs");
+const path = require("path");
+
+function clearDir(dir){
+    fs.readdir(dir, (err, files) => {
+        if (err) {
+            throw err;
+        }
+        for (const file of files) {
+            console.log(path.sep);
+            fs.rmSync(dir + path.sep + file, {force:true, recursive:true});
+        }
+    });
+}
+
+function buildCSS(cb) {
+    console.log("CSS");
+    const css = "css";
+    clearDir(css);
+    gulp.src('./src/scss/main.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename("style.css"))
+        .pipe(autoprefixer({
+            flexbox:true,
+            grid:true,
+        }))
+        .pipe(gulp.dest("." + path.sep + css));
+
+    cb();
+}
+
+function buildHTML(cb) {
+    const indexPath = "." + path.sep + "index.html"
+    console.log(indexPath);
+    if (!fs.existsSync(indexPath)) {
+        console.log("There is no index.html");
+        return cb();
+    }
+    gulp.src(indexPath)
+    .pipe(htmlmin({
+        collapseWhitespace:true,
+        collapseBooleanAttributes:true,
+        minifyCSS:true,
+        minifyJS:true,
+    }))
+    .pipe(gulp.dest("."+ path.sep + "dist"));
+    
+    cb();
+}
+
+function buildJS(cb) {
+    console.log("JS");
+    const jsPath = `.${path.sep}src${path.sep}js${path.sep}`;
+    if (!fs.existsSync(jsPath)) {
+        console.log("There is no JS sources");
+        return cb();
+    }
+    gulp.src(`${jsPath}*.js`)
+        .pipe(minify())
+        .pipe(gulp.dest(`.${path.sep}dist${path.sep}js`))
+    cb();
+}
+
+function run (cb) {
+    clearDir("css");
+    gulp.src("./src/scss/main.scss")
+        .pipe(rename("style.css"))
+        .pipe(sourcemap.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemap.write("maps"))
+        .pipe(gulp.dest('css'))
+        .on("end", cb);
+
+}
+
+
+exports.build = gulp.series(buildHTML, buildCSS, buildJS);
+
+exports.css = buildCSS;
+    
+exports.run = ()=>{
+    run(console.log);
+    gulp.watch('./src/**/*', run);
+
+};
